ca9090eb830116b35d5b7f3d9b39c860332a595c
Ojas <ojastulswani@gmail.com>
2024-09-19 12:05:08 +0530

    Dynamic Image Rendering

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,14 +1,25 @@
 import useProducts from "../hooks/useProducts";
 
+
 const ProductList = () => {
   const { products } = useProducts();
   console.log(products)
+  
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {products.map((product) => (
         <div key={product.sku} className="bg-white shadow-md rounded-lg p-4">
-          <img src={`/assets${product.sku}.jpg`} alt="image" />
+          
+          {/* <img src={require(`../assets/${product.sku}.jpg`)} alt="image" /> */}
+
+          {/* dynamic image */}
+          <img src={new URL(`../assets/${product.sku}.jpg`, import.meta.url).href} 
+          alt="image"
+          className="w-full object-cover rounded-lg mx-auto"/>
+
+          
+
           <h2 className="text-lg font-bold">{product.name}</h2>
           <p>SKU: {product.sku}</p>
           <p>Price: ${product.price}</p>
